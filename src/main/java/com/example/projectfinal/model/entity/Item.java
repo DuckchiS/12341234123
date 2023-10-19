e4c33c82af125ce7e0905d30c2290c1b6525e601
dc <rjsdn1862@daum.net>
2023-10-19 15:55:38 +0900

    1

---
+++ src/main/java/com/example/projectfinal/model/entity/Item.java
@@ -0,0 +1,33 @@
+package com.example.projectfinal.model.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "item", schema = "items_schema")
+public class Item {
+    @Id
+    private int ID;
+    private String Item_Name;
+
+    @ManyToOne
+    private Category category;
+
+    @OneToMany(mappedBy = "item", fetch = FetchType.LAZY)
+    List<Orders> orders = new ArrayList<>();
+
+    private int price;
+
+    private int Quantity;;
+}
---
+++ src/main/java/com/example/projectfinal/service/WebService.java
@@ -0,0 +1,65 @@
+package com.example.projectfinal.service;
+
+import com.example.projectfinal.model.entity.Item;
+import com.example.projectfinal.model.entity.Orders;
+import com.example.projectfinal.repository.*;
+import com.example.projectfinal.vo.OderVOList;
+import com.example.projectfinal.vo.OrderVO;
+import com.example.projectfinal.vo.Sale_RateVO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import java.util.stream.Collectors;
+
+@Service
+public class WebService {
+    private OrderRepository orderRepository;
+    private ItemRepository itemRepository;
+
+    public WebService(
+                      OrderRepository orderRepository,
+                      ItemRepository itemRepository){
+        this.orderRepository = orderRepository;
+        this.itemRepository = itemRepository;
+    }
+
+
+
+    public OderVOList Orders(Pageable pageable){
+        Page<Orders> result = orderRepository.findAll(pageable);
+
+
+
+        //판매량 예측보다 현재 재고가 적으면
+        List<OrderVO> orders = result.stream().map(
+                orderEntity -> OrderVO.builder()
+                        .date(orderEntity.getDate())
+                        .i_name(orderEntity.getItem().getItem_Name())
+                        .quantity(orderEntity.getOrder_Quantity())
+                        .State(orderEntity.getOrder_State())
+                        .build()
+        ).collect(Collectors.toList());
+
+        OderVOList oderVOList = OderVOList.builder()
+                .orderVOList(orders)
+                .totalPage(result.getTotalPages())
+                .build();
+        return oderVOList;
+    }
+
+    public Page<Item> Items(Pageable pageable){
+        Page<Item> items = itemRepository.findAll(pageable);
+
+        return items;
+    }
+
+    public Item Item(int id){
+        Item item = itemRepository.findById(id).get();
+
+        return item;
+    }
+}
